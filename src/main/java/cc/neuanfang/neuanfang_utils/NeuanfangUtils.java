979e3745d89869ed7837b85a578271952647a87d
FildCommander <git@paul-stoeckel.de>
2024-10-13 00:11:04 +0200

    Initial commit

---
+++ src/main/java/cc/neuanfang/neuanfang_utils/NeuanfangUtils.java
@@ -0,0 +1,15 @@
+package cc.neuanfang.neuanfang_utils;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NeuanfangUtils implements ModInitializer {
+	public static final String MOD_ID = "neuanfang_utils";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/cc/neuanfang/neuanfang_utils/NeuanfangUtilsClient.java
@@ -0,0 +1,11 @@
+package cc.neuanfang.neuanfang_utils;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NeuanfangUtilsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
